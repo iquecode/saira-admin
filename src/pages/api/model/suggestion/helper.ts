b603c1662a2e2ea9c9a59d813bae6a405aa66257
iquecode <iquecode@gmail.com>
2022-07-12 23:05:28 -0300

    redeploy prisma migrate deploy

--- src/pages/api/model/suggestion/helper.ts
+++ src/pages/api/model/suggestion/helper.ts
@@ -4,7 +4,7 @@ import { client } from '../../lib/prisma/client';
 export async function createSuggestion(userId:string, type: string, description: string) 
 {
 
-        console.log('####aqui data');
+        console.log('###@#aqui data');
         console.log('userId: ' + userId);
         console.log('type :' + type);
         console.log('description: ' + description);
